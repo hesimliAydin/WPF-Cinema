3313ff0cff35ae931ca7e7fef6688afeb14fa322
ZoreX <aydinhsimli@gmail.com>
2022-12-07 02:04:12 +0400

    done

---
+++ WPF-Cinema/Model/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Cinema.Model
+{
+    public class Movie
+    {
+        public string? Poster { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public string? Year { get; set; }
+        public string? Writer { get; set; }
+        public string? Actors { get; set; }
+    }
+}
